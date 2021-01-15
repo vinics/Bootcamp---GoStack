b6227697cdc79175ecde6a9e57c973ac958d0b31
vinics <vinics.dr@gmail.com>
2021-01-15 20:31:48 -0300

    Create frontend interface

---
+++ 04-gobarber-web/src/components/Button/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.button`
+  background: #ff9000;
+  color: #312e38;
+  font-weight: 500;
+
+  height: 56px;
+  width: 100%;
+
+  padding: 0 16px;
+  border-radius: 10px;
+  border: 0;
+  margin-top: 16px;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: ${shade(0.2, '#ff9000')};
+  }
+`;
---
+++ 04-gobarber-web/src/pages/SignUp/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data)
+  }
+
+  return (
+    <Container>
+    <Background />
+
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Name" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password "
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit">Cadastrar</Button>
+      </Form>
+
+      <a href="create">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+  )
+};
+
+export default SignUp;
