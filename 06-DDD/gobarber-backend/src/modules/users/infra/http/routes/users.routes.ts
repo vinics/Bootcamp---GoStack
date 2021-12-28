c0ec65d99711f639c619621510168cdaf63b016e
vinics <vinics.dr@gmail.com>
2021-12-28 16:33:42 -0300

    04-Configurando Imports

---
+++ 06-DDD/gobarber-backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+  const { name, email, password } = request.body;
+
+  const CreateUser = new CreateUserService();
+
+  const user = await CreateUser.execute({ name, email, password });
+
+  delete user.password;
+
+  return response.json(user);
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  },
+);
+
+export default usersRouter;
---
+++ 06-DDD/gobarber-backend/src/shared/infra/http/server.ts
@@ -0,0 +1,42 @@
+import 'reflect-metadata';
+
+import cors from 'cors';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import routes from './routes';
+import uploadConfig from '@config/upload';
+
+import '@shared/infra/typeorm';
+
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
+
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log(';-) Server up on port 3333');
+});
