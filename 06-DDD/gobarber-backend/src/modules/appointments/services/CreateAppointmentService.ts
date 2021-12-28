a5727daa2fae8b0c464a8478146ff4dc31e61f2c
vinics <vinics.dr@gmail.com>
2021-12-28 17:14:24 -0300

    04-Liskov

--- 06-DDD/gobarber-backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ 06-DDD/gobarber-backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -24,12 +24,11 @@ class CreateAppointmentService {
     if (findAppointmentInSameDate)
       throw new AppError('This appointment is already booked');
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
 
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
