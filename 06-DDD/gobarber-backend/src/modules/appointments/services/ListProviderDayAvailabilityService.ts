1788922af1f33ea195a843f2ae459e3e419eb117
vinics <vinics.dr@gmail.com>
2022-02-28 18:25:43 -0300

    06 - Removing old appointments

--- 06-DDD/gobarber-backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ 06-DDD/gobarber-backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-useless-constructor */
 import { injectable, inject } from 'tsyringe';
-import { getDate, getDaysInMonth, getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -46,14 +46,18 @@ class ListProviderDayAvailabilityService {
       (value, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
 
